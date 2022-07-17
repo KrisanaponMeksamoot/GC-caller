6a6c2fd5e052a4ddf8924fefad4b400f692b6188
KrisanaponMeksamoot <kriskrisanapon@gmail.com>
2022-07-17 11:03:05 +0700

    for forge1.12.2

--- src/main/java/net/kris/gc_caller/GCCaller.java
+++ src/main/java/net/kris/gc_caller/GCCaller.java
@@ -1,51 +1,26 @@
 package net.kris.gc_caller;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
-import org.lwjgl.glfw.GLFW;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+@Mod(modid = GCCaller.MODID, name = GCCaller.NAME, version = GCCaller.VERSION)
+public class GCCaller {
+    public static final String MODID = "gc_caller";
+    public static final String NAME = "Garbage Collector Caller";
+    public static final String VERSION = "1.0.0";
 
-public class GCCaller implements ClientModInitializer {
-	public static final String MODID = "gc_caller";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    private static Logger logger;
 
-	private KeyBinding keyBinding;
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
 
-	@Override
-	public void onInitializeClient() {
-		GCCallerThread.getInstance();
-		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"key.gc_caller.call_gc",
-				InputUtil.Type.KEYSYM,
-				GLFW.GLFW_KEY_UNKNOWN,
-				"category.gc_caller.name"));
-		ClientTickEvents.END_CLIENT_TICK.register(this::onEndClientTick);
-		ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
-			GCCallerThread.gc();
-		});
-		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-			GCCallerThread.gc();
-		});
-	}
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
 
-	private void onEndClientTick(MinecraftClient client) {
-		while (keyBinding.wasPressed()) {
-			if (client.player == null) {
-				GCCallerThread.gc();
-			} else if (GCCallerThread.gc()) {
-				client.player.sendMessage(Text.translatable("message.gc_caller.gc_called"), true);
-			} else {
-				client.player.sendMessage(Text.translatable("message.gc_caller.gc_already_called"), true);
-			}
-		}
-	}
+    }
 }
